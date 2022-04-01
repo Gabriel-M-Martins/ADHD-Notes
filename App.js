8299a500fe017dcdaab825d33d8eeb3a7f898dcf
Gabriel-M-Martins <gabrielmartinesco@gmail.com>
2022-04-01 02:28:22 -0300

    Reorganized navigation stacks

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import "react-native-gesture-handler";
 import React from "react";
 //import { EStatusBar } from "expo-status-bar";
 
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+//import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer, useRoute } from "@react-navigation/native";
 
 import {
   Home,
@@ -15,88 +16,81 @@ import {
   DailyTasks,
   IsolatedNotes,
   DiaryNotes,
+  OpenedDailyTask,
 } from "./app/screens/Index";
 
 import setIcon from "./app/utils/SetIcon";
 
-const Drawer = createDrawerNavigator();
-const MainTab = createBottomTabNavigator();
-const NotesTab = createMaterialTopTabNavigator();
-const TasksTab = createMaterialTopTabNavigator();
+const MainNav = createNativeStackNavigator();
+const TabsNav = createMaterialBottomTabNavigator();
+const NotesNav = createMaterialTopTabNavigator();
+const TasksNav = createMaterialTopTabNavigator();
 
 function TasksTabs() {
   return (
-    <TasksTab.Navigator screenOptions={{ tabBarStyle: { paddingTop: "5%" } }}>
-      <TasksTab.Screen
+    <TasksNav.Navigator screenOptions={{ tabBarStyle: { paddingTop: "5%" } }}>
+      <TasksNav.Screen
         name="DailyTasks"
         component={DailyTasks}
         options={{ title: "Diárias" }}
       />
-      <TasksTab.Screen
+      <TasksNav.Screen
         name="PunctualTasks"
         component={PunctualTasks}
         options={{ title: "Pontuais" }}
       />
-    </TasksTab.Navigator>
+    </TasksNav.Navigator>
   );
 }
 
 function NotesTabs() {
   return (
-    <NotesTab.Navigator screenOptions={{ tabBarStyle: { paddingTop: "5%" } }}>
-      <NotesTab.Screen
+    <NotesNav.Navigator screenOptions={{ tabBarStyle: { paddingTop: "5%" } }}>
+      <NotesNav.Screen
         name="DiaryNotes"
         component={DiaryNotes}
         options={{ title: "Diárias" }}
       />
-      <NotesTab.Screen
+      <NotesNav.Screen
         name="IsolatedNotes"
         component={IsolatedNotes}
         options={{ title: "Isoladas" }}
       />
-    </NotesTab.Navigator>
+    </NotesNav.Navigator>
   );
 }
 
 function HomeTabs() {
   return (
-    <MainTab.Navigator
+    <TabsNav.Navigator
       initialRouteName="Home"
       screenOptions={({ route }) => ({
         tabBarIcon: ({ focused, color, size }) =>
           setIcon(route.name, focused, color, size),
-        tabBarActiveTintColor: "tomato",
-        tabBarInactiveTintColor: "gray",
-        headerShown: false,
       })}
     >
-      <MainTab.Screen name="Notes" component={NotesTabs} />
-      <MainTab.Screen name="Home" component={Home} />
-      <MainTab.Screen name="Tasks" component={TasksTabs} />
-    </MainTab.Navigator>
+      <TabsNav.Screen name="Notes" component={NotesTabs} />
+      <TabsNav.Screen name="Home" component={Home} />
+      <TabsNav.Screen name="Tasks" component={TasksTabs} />
+    </TabsNav.Navigator>
+  );
+}
+
+function Main() {
+  return (
+    <MainNav.Navigator screenOptions={{ headerShown: false }}>
+      <MainNav.Screen name="Home" component={HomeTabs} />
+      <MainNav.Screen name="Profile" component={Profile} />
+      <MainNav.Screen name="Settings" component={Settings} />
+      <MainNav.Screen name="OpenedDailyTask" component={OpenedDailyTask} />
+    </MainNav.Navigator>
   );
 }
 
 export default function App() {
   return (
     <NavigationContainer>
-      {/* <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen
-          name="Tabs"
-          component={HomeTabs}
-          options={{
-            title: "Home",
-            headerTitle: () => {
-              return;
-            },
-          }}
-        />
-
-        <Drawer.Screen name="Profile" component={Profile} />
-
-        <Drawer.Screen name="Settings" component={Settings} />
-      </Drawer.Navigator> */}
-      <HomeTabs />
+      <Main />
     </NavigationContainer>
   );
 }
--- app/screens/Index.js
+++ app/screens/Index.js
@@ -3,5 +3,6 @@ export { default as Profile } from "./Profile";
 export { default as Settings } from "./Settings";
 export { default as IsolatedNotes } from "./IsolatedNotes";
 export { default as DiaryNotes } from "./DiaryNotes";
-export { default as PunctualTasks } from "./PunctualTasks";
-export { default as DailyTasks } from "./DailyTasks";
+export { default as PunctualTasks } from "./Tasks/PunctualTasks";
+export { default as DailyTasks } from "./Tasks/DailyTasks";
+export { default as OpenedDailyTask } from "./Tasks/OpenedDailyTask";
